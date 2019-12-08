4dda5199f8e72fb96c4ef2593112b588467cd2fa
George <51100451+PorgiesPie@users.noreply.github.com>
2019-12-08 11:46:09 +0000

    Create Persistent Bugger.java

---
+++ 6 Kyu/Persistent Bugger.java
@@ -0,0 +1,32 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence,
+// which is the number of times you must multiply the digits in num until you reach a single digit.
+
+import java.util.ArrayList;
+public class Persist {
+    static int count = 0;
+    public static int persistence(long n) {
+        long digit;
+        if (n <= 9) return 0;
+        else digit = multiply(n);
+        if (digit <= 9) {
+            int temp = count;
+            count = 0;
+            return temp;
+        } else {
+            return persistence(digit);
+        }
+    }
+    public static long multiply(long n) {
+        count++;
+        long sum = 1;
+        ArrayList<Long> arr = new ArrayList<>();
+        while (n > 0) {
+            arr.add(n%10);
+            n/=10;
+        }
+        for (long l : arr) {
+            sum *= l;
+        }
+        return sum;
+    }
+}

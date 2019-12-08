c586e0be5bc594682d6ebab4decb9863dc63e57a
George <51100451+PorgiesPie@users.noreply.github.com>
2019-12-08 11:53:34 +0000

    Create Multiples of 3 or 5.java

---
+++ 6 Kyu/Multiples of 3 or 5.java
@@ -0,0 +1,26 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+//    Note: If the number is a multiple of both 3 and 5, only count it once.
+
+import java.util.*;
+public class Solution {
+
+  public int solution(int number) {
+    HashSet<Integer> set = new HashSet<>();
+    int sum = 0;
+    
+    for (int i = 1; i < number; i++){
+      if(i % 3 == 0){
+        set.add(i);
+      } else if (i % 5 == 0){
+        set.add(i);
+      }
+    }
+    
+    for (int num : set){
+      sum += num;
+    }
+    
+    return sum;
+  }
+}
